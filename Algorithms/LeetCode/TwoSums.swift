fd30aaec447fc2a5a61343e15304751d7ec297e2
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-10-24 01:08:25 -0500

    before app code

---
+++ Algorithms/LeetCode/TwoSums.swift
@@ -0,0 +1,14 @@
+//
+//  TwoSums.swift
+//  Algorithms
+//
+//  Created by Evgeniy Poznyak on 10/24/20.
+//
+
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        return [1]
+    }
+}
---
+++ AlgorithmsTests/LeetCode/TwoSumsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwoSumsTests.swift
+//  AlgorithmsTests
+//
+//  Created by Evgeniy Poznyak on 10/24/20.
+//
+
+import XCTest
+@testable import Algorithms
+
+class TwoSumsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testShouldReturn0and1() throws {
+        // [InlineData(new[] {2, 7, 11, 15}, 9, new[] {0, 1})]
+       let sut = Solution().twoSum([2, 7, 11, 15], 9)
+        XCTAssertEqual(sut, [0, 1], "Should twoSum return [0, 1]")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
